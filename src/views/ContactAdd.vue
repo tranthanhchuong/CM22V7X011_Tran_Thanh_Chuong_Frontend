90345ce149ced9d582d1a7a97287a5d251ce39e0
Chuong tran <Tranchuong@gmail.com>
2024-09-30 20:11:36 +0700

    contact frontend

---
+++ src/views/ContactAdd.vue
@@ -0,0 +1,36 @@
+<template>
+    <div v-if="contact" class="page">
+        <h4>Thêm liên hệ</h4>
+        <ContactForm :contact="contact" @submit:contact="createContact" />
+        <p>{{ message }}</p>
+    </div>
+</template>
+<script>
+import ContactForm from "@/components/ContactForm.vue";
+import ContactService from "@/services/contact.service";
+export default {
+    components: {
+        ContactForm,
+    },
+    data() {
+        return {
+            contact: null,
+            message: "",
+        };
+    },
+    methods: {
+        async createContact(data) {
+            try {
+                await ContactService.create(data);
+                this.message = "Liên hệ được thêm thành công.";
+            } catch (error) {
+                console.log(error);
+            }
+        },
+    },
+    created() {
+        this.contact = {};
+        this.message = "";
+    },
+};
+</script>
